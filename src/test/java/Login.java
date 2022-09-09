affd89b24add896be7bef4ed693a8ef63efb5bf6
rajansa <saravana.karthik.rajan@kalmarglobal.com>
2022-09-09 15:52:38 +0530

    TestAutomationKT

---
+++ src/test/java/Login.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class Login {
+
+    @Test
+    public void login() {
+        System.out.println("test");
+    }
+
+    @Test
+    public void loginPage() {
+        System.out.println("testpage");
+    }
+}
+
