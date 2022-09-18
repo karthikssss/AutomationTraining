b1b0ed83bd23b706d397bdea03ab267440b8868e
Pradap Pandiyan <pradap@axis.xyz>
2022-09-19 00:29:48 +0400

    added test to run on the ci

--- src/test/MainTest/DateAndTimeCalculator.java
+++ src/test/MainTest/DateAndTimeCalculator.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
@@ -28,8 +29,12 @@ public class DateAndTimeCalculator {
     @BeforeSuite
     public void triggerSetUp()
     {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
         WebDriverManager.chromedriver().setup();
-        driver= new ChromeDriver();
+        driver= new ChromeDriver(options);
         launchScreen= new LaunchScreen(driver);
         signInPopUp= new SignInPopUp(driver);
         customScreen= new CustomScreen(driver);
